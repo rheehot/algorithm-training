c1ae45e53d3be970f6e6980710341db6481c80a4
kimchanjung <mogomezwai@gmail.com>
2019-10-15 00:06:49 +0900

    FormingMagicSquare

---
+++ src/main/java/algorithms/ElectronicsShop.java
@@ -0,0 +1,21 @@
+package main.java.algorithms;
+
+/**
+ * https://www.hackerrank.com/challenges/electronics-shop/problem
+ */
+
+public class ElectronicsShop {
+    public static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+
+        int result = -1;
+
+        for (int i = 0; i < keyboards.length ; i++) {
+            for (int j = 0; j < drives.length ; j++) {
+                int sum = keyboards[i] + drives[j];
+                result = sum <= b && sum > result ? sum : result;
+            }
+        }
+
+        return result;
+    }
+}
