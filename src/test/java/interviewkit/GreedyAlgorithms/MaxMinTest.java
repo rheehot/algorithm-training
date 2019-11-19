c06442114e6bb69196e546dc7716b7ccc5bda084
kimchanjung <mogomezwai@gmail.com>
2019-11-19 21:45:15 +0900

    MaxMin

---
+++ src/test/java/interviewkit/GreedyAlgorithms/MaxMinTest.java
@@ -0,0 +1,40 @@
+package test.java.interviewkit.GreedyAlgorithms;
+
+import main.java.interviewkit.GreedyAlgorithms.MaxMin;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxMinTest {
+
+    @Test
+    public void test() {
+        //Given
+        int[] input = {4504
+                ,1520
+                ,5857
+                ,4094
+                ,4157
+                ,3902
+                ,822
+                ,6643
+                ,2422
+                ,7288
+                ,8245
+                ,9948
+                ,2822
+                ,1784
+                ,7802
+                ,3142
+                ,9739
+                ,5629
+                ,5413
+                ,7232};
+
+        //When
+        int result = MaxMin.maxMin(5, input);
+
+        //Then
+        assertEquals(1335, result);
+    }
+}
---
+++ src/test/java/smallFunctions/StreamTest.java
@@ -0,0 +1,16 @@
+package test.java.smallFunctions;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class StreamTest {
+
+    @Test
+    public void 배열에서_특정개수까지만_따로저장() {
+        int[] input = {10, 100, 300, 200, 1000, 20, 30};
+        int[] newArr = IntStream.of(input).limit(3).toArray();
+        Arrays.stream(newArr).forEach(v -> System.out.println(v));
+    }
+}
