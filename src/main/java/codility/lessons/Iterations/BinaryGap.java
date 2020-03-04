6cf28a7b2fbec6b966168407db3d842dab508080
kimchanjung <mogomezwai@gmail.com>
2020-03-04 19:45:20 +0900

    [codility] binary cap

---
+++ src/main/java/codility/lessons/Iterations/BinaryGap.java
@@ -0,0 +1,27 @@
+package codility.lessons.Iterations;
+
+/**
+ * Created by kimchanjung on 2020-03-04 7:23 오후
+ * https://app.codility.com/c/run/trainingWQKSG9-7JQ/
+ * [Binary Gap]
+ * 숫자를 이진수로 변경후 0이 연속되는 가장 큰 자리 수를 리턴
+ * 1041 => 10000010001
+ * 00000 , 000 중 0이 5개가 되는 부분이 가장 크므로 5를 리턴한다.
+ */
+public class BinaryGap {
+    public static int solution(int N) {
+        String s = Integer.toBinaryString(N);
+        int count = 0, max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ src/test/java/codility/lessons/Iterations/BinaryGapTest.java
@@ -0,0 +1,17 @@
+package codility.lessons.Iterations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kimchanjung on 2020-03-04 7:29 오후
+ */
+public class BinaryGapTest {
+
+    @Test
+    public void test() {
+
+        assertEquals(5, BinaryGap.solution(1041));
+    }
+}
